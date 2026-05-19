20e7e3ccc60980682aacdb3b5900956bb9cf5064
antonella-medina <antonellaxanax@gmail.com>
2026-05-18 23:01:20 -0300

    agregado componente Nav de la Parte 1 de tp3

---
+++ src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Proyectos</a></li>
+        <li><a href="#">Contacto</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
