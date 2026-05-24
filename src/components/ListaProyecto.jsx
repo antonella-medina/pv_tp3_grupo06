ec70acfcd3e0bb6f767fc694db0c9719de581b16
LuisMiguelMorenoSivila <morenosivilaluismiguel@gmail.com>
2026-05-24 00:42:17 -0300

    fecha y hora

--- src/components/ListaProyecto.jsx
+++ src/components/ListaProyecto.jsx
@@ -1,14 +1,15 @@
 import proyectoService from "../services/proyectoService";
-import {useState} from "react";
+import {useState,useEffect} from "react";
 import ProyectoCard from "./ProyectoCard.jsx"; 
 import DetalleProyecto from "./DetalleProyecto.jsx";
 import FormularioProyecto from "./FormularioProyecto.jsx";
+import RegistroActividad from "./RegistroActividad.jsx";
 
 const ListaProyecto =() => {
   const [proyectos, setProyectos] = useState(proyectoService.obtenerProyectos());
   const [busqueda, setBusqueda] = useState("");
   const [proyectoSeleccionado, setProyectoSeleccionado] = useState(null);
-
+  const [fechaActualizacion, setFechaActualizacion] = useState(null);
     /*recibimos el proyecto hijo */
     const handleAgregarProyectoPadre = (nuevoProyecto) => {
       proyectoService.agregarProyecto(nuevoProyecto);
@@ -29,6 +30,21 @@ const ListaProyecto =() => {
       const proyecto = proyectoService.obtenerProyectoPorId(id);
       setProyectoSeleccionado(proyecto);
     }
+
+    useEffect(() => {
+      const ahora = new Date();
+     
+      const dia = String(ahora.getDate()).padStart(2, "0");
+      const mes = String(ahora.getMonth() + 1).padStart(2, "0");
+      const anio = ahora.getFullYear();
+      const horas = String(ahora.getHours()).padStart(2, "0");
+      const minutos = String(ahora.getMinutes()).padStart(2, "0");
+      const fechaFormateada = `${dia}/${mes}/${anio} a las ${horas}:${minutos} hs.`;
+    
+      setFechaActualizacion(fechaFormateada);
+    }, [proyectos]);
+    
+    
 
     /*renderizado condicional para la vista de detalle */
     if (proyectoSeleccionado){
@@ -66,10 +82,10 @@ const ListaProyecto =() => {
               onVerDetalle={handleVerDetalle} 
               onEliminar={handleEliminar}/>
           ))}
-
-        </div>
         </div>
 
-  );
-}
+        <RegistroActividad fecha={fechaActualizacion} />
+      </div>
+      );
+      };
 export default ListaProyecto;
