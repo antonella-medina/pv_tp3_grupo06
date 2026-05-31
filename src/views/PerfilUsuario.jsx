fd85b46f04c881f982f0320f58356a2264d212ce
LuisMiguelMorenoSivila <morenosivilaluismiguel@gmail.com>
2026-05-31 17:19:46 -0300

    Dashboard y PerfilUsuario

---
+++ src/views/PerfilUsuario.jsx
@@ -0,0 +1,33 @@
+import { Container, Typography, Paper, List, ListItem, ListItemText } from "@mui/material";
+
+const PerfilUsuario = () => {
+  const usuario = {
+    nombre: "Luis Pérez",
+    rol: "Alumno",
+    institucion: "Universidad Nacional de Jujuy"
+  };
+
+  return (
+    <Container>
+      <Typography variant="h4" gutterBottom sx={{ mt: 4, color:"black" }}>
+        Perfil del Usuario
+      </Typography>
+
+      <Paper elevation={3} sx={{ p: 3 }}>
+        <List>
+          <ListItem>
+            <ListItemText primary="Nombre" secondary={usuario.nombre} />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="Rol" secondary={usuario.rol} />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="Institución" secondary={usuario.institucion} />
+          </ListItem>
+        </List>
+      </Paper>
+    </Container>
+  );
+};
+
+export default PerfilUsuario;
